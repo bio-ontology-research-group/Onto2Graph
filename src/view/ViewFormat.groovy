ebc4d08c8610098bea7ff717cf83389aa76b55a5
MiguelAngelRG <mangelrg21@gmail.com>
2015-10-19 13:40:57 +0300

    Some changes

--- src/view/ViewFormat.groovy
+++ src/view/ViewFormat.groovy
@@ -1,5 +1,8 @@
 package view
 
+import edu.uci.ics.jung.graph.DirectedSparseGraph
+import edu.uci.ics.jung.graph.Graph
+import org.omg.CORBA.Request
 import org.semanticweb.owlapi.model.OWLClass
 import org.semanticweb.owlapi.model.OWLOntology
 import org.semanticweb.owlapi.reasoner.OWLReasoner
@@ -11,8 +14,6 @@ import tool.RequestManager;
  * @version 1.0
  */
 public abstract class ViewFormat {
-    protected int classesIndex=0;
-    protected int classesCounter = 0;
     protected BufferedWriter output=null;
 
     public ViewFormat(){
@@ -24,22 +25,105 @@ public abstract class ViewFormat {
      * @return
      */
     public void parseOntology(OWLOntology ontology,OWLReasoner reasoner, String[] properties,String fileOutPath) {
+        if((ontology!=null)&&(reasoner!=null)) {
+            properties = null;
+            Graph graph = this.buildGraph(ontology, reasoner, properties);
+            this.serializeGraph(fileOutPath,graph);
+        }
+    }
+
+    private HashMap buildNode(OWLClass rootClass){
+        HashMap root = null;
+        if(rootClass!=null) {
+            root = new HashMap();
+            root.put("owlClass",rootClass.getIRI().toString());
+            root.put("classURI","<"+rootClass.getIRI().toString()+">");
+            root.put("remainder",rootClass.getIRI().getFragment().toString());
+            root.put("label","owl:Thing");
+            root.put("deprecated",false)
+        }
+        return(root)
+    }
+
+    private Graph buildGraph(OWLOntology ontology,OWLReasoner reasoner,String[] properties){
+        DirectedSparseGraph graph= new DirectedSparseGraph<>();
+        Set<OWLClass> classes = ontology.getClassesInSignature(true);
+        int classesIndex = 0;
+        int classesCounter = ontology.getClassesInSignature(true).size();
+        classes.remove(ontology.getOWLOntologyManager().getOWLDataFactory().getOWLNothing());
+        Iterator<OWLClass> it = classes.iterator();
+        OWLClass clazz;
+        HashMap subClass;
+        HashMap root;
+        while(it.hasNext()){
+            clazz = it.next();
+            root = buildNode(clazz);
+            classesIndex++;
+            printProgressBar((int)Math.round((classesIndex*100)/(classesCounter)),"building the graph...");
+            graph.addVertex(root);
+            Set<HashMap> subClasses = RequestManager.getInstance().runQuery(root.get("classURI").toString(),ontology,reasoner,"subclass",true,false);
+            Iterator<HashMap> jt = subClasses.iterator();
+            while(jt.hasNext()){
+                subClass = jt.next();
+                graph.addVertex(subClass);
+                graph.addEdge(graph.edgeCount+1,root,subClass);
+            }
+            if((properties!=null)&&(properties.length>0)){
+                String objectProperty;
+                for(int i=0;i<properties.length;i++){
+                    objectProperty = properties[i];
+                    Set<HashMap> result = RequestManager.getInstance().relationQuery(objectProperty,root.get("classURI").toString(),ontology);
+                    Iterator<HashMap> kt = result.iterator();
+                    HashMap objectPropertyClass;
+                    while(kt.hasNext()){
+                        objectPropertyClass = kt.next();
+                        graph.addVertex(objectPropertyClass);
+                        graph.addEdge(objectProperty,root,objectPropertyClass);
+                    }
+                }
+            }
+        }
+        return(graph);
+    }
+
+    private static void printProgressBar(int percent,String message){
+        StringBuilder bar = new StringBuilder("[");
+
+        for(int i = 0; i < 50; i++){
+            if( i < (percent/2)){
+                bar.append("=");
+            }else if( i == ((int)percent/2)){
+                bar.append(">");
+            }else{
+                bar.append(" ");
+            }
+        }
+
+        bar.append("]   " + percent + "%\t"+message);
+        System.out.print("\r" + bar.toString());
+    }
+
+    private void serializeGraph(String fileOutPath,Graph graph){
         if((fileOutPath!=null)&&(!fileOutPath.isEmpty())){
             try {
-                classesIndex = 0;
-                classesCounter = ontology.getClassesInSignature(true).size();
-                HashMap rootClass = buildRoot(ontology.getOWLOntologyManager().getOWLDataFactory().getOWLThing());
+                System.out.println("Edges: "+graph.getEdgeCount());
+                System.out.println("Vertex: "+graph.getVertexCount());
                 File file = new File(fileOutPath+getExtension());
                 output = new BufferedWriter(new FileWriter(file));
                 if(output!=null){
-                    System.out.println(classesCounter);
-                    output.append(this.getHeader());
-                    /*if ((properties != null) && (properties.length != 0)){
-                        this.recursiveQueryProperties(ontology,reasoner,rootClass,properties);
-                    }else{*/
-                        this.recursiveQuery(ontology, reasoner,rootClass,0);
-                    //}
-                    output.append(this.getFooter());
+                    if(graph!=null){
+                        Iterator it = graph.getVertices().iterator();
+                        HashMap vertice;
+                        while(it.hasNext()){
+                            vertice = it.next();
+                            Iterator jt = graph.getOutEdges(vertice);
+                            Object edge;
+                            while(jt.hasNext()){
+                                edge = jt.next();
+                                System.out.println(vertice+"-->"+edge);
+                            }
+                        }
+                    }
                 }
 
             } catch ( IOException e ) {
@@ -53,84 +137,6 @@ public abstract class ViewFormat {
             System.out.println("The file was not created");
             System.exit(-1);
         }
-    }
-
-    private HashMap buildRoot(OWLClass rootClass){
-        HashMap root = null;
-        if(rootClass!=null) {
-            root = new HashMap();
-            root.put("owlClass",rootClass.getIRI().toString());
-            root.put("classURI","<"+rootClass.getIRI().toString()+">");
-            root.put("remainder",rootClass.getIRI().getFragment().toString());
-            root.put("label","owl:Thing");
-            root.put("deprecated",false)
-        }
-        return(root)
-    }
-
-    private void recursiveQuery(OWLOntology ontology,OWLReasoner reasoner,HashMap root,int level){
-        Set<HashMap> subClasses = RequestManager.getInstance().runQuery(root.get("classURI").toString(),ontology,reasoner,"subclass",true,false);
-        if((subClasses.isEmpty())||(level>1)){
-            return;
-        }
-        output.append(formatter(root,subClasses));
-        HashMap child;
-        for(int i=0;i<subClasses.size();i++){
-            child = subClasses.getAt(i);
-            if(child!=null){
-                classesIndex++;
-                recursiveQuery(ontology,reasoner,child,level+1);
-                System.out.print("\r"+"Number of classes transformed:"+classesIndex);
-            }
-        }
-    }
-
-    private void recursiveQueryProperties(OWLOntology ontology,OWLReasoner reasoner, OWLClass rootClass,String[] objectProperties){
-        String objectProperty;
-        Set<OWLClass>subClasses = new HashSet<OWLClass>();
-        Set<OWLClass>result = null;
-        for(int i=0;i<objectProperties.length;i++){
-            objectProperty = objectProperties[i];
-            result = RequestManager.getInstance().relationQuery(objectProperty,rootClass.getIRI().toString(),ontology);
-            if((result!=null)&&(!result.isEmpty())){
-                output.append(formatter(rootClass,result,objectProperty))
-                subClasses.addAll(result);
-            }
-        }
-        if((subClasses==null)||(subClasses.isEmpty())){
-            return;
-        }
-
-        classesIndex++;
-        printProgressBar((int)Math.round((classesIndex*100)/(ontology.getClassesInSignature(true).size())));
-
-
-        Iterator<OWLClass> it = subClasses.iterator();
-        OWLClass child;
-        while(it.hasNext()){
-            child = it.next();
-            if(child!=null){
-                recursiveQueryProperties(ontology,reasoner,child,objectProperties);
-            }
-        }
-        return;
-    }
-
-    private static void printProgressBar(int percent){
-        StringBuilder bar = new StringBuilder("[");
-
-        for(int i = 0; i < 50; i++){
-            if( i < (percent/2)){
-                bar.append("=");
-            }else if( i == ((int)percent/2)){
-                bar.append(">");
-            }else{
-                bar.append(" ");
-            }
-        }
-
-        bar.append("]   " + percent + "%     ");
-        System.out.print("\r" + bar.toString());
     }
 
     public abstract String getHeader();
